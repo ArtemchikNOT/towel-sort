d9d1778c1f44ceb1bd5f3cec40286392f47b4b8a
ArtemchikNOT <artemchuro@mail.ru>
2020-03-11 23:44:20 +0300

    second comm

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+	if (i%2==1) {
+		for (var i=0; i<=4; i++){
+			for (var j=0; j<=4; j++){
+				matrix.join().split(",");
+			}
+		}
+	} else {
+		matrix.join().split(",");
+	}
   return [];
 }
